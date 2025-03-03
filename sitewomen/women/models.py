b280c25a7c071ecd4da06cf6ecc09568d124abce
asb <asbabushkin@yandex.ru>
2025-03-03 17:32:54 +0500

    4.2 Создание связи many-to-one.

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -19,6 +19,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     objects = models.Manager()
     published = PublishedManager()
@@ -33,6 +34,14 @@ class Women(models.Model):
     # формирует адрес для каждой записи бд
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_slug": self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 
 
