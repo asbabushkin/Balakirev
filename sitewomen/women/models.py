49712ff39ace801ef0318eb40d516e3ed33bc45e
asb <asbabushkin@yandex.ru>
2025-03-05 13:51:32 +0500

    4.6 Добавление тэгов.

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -20,7 +20,7 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts")
-    tags = models.ManyToManyField("TagPost", blank=True, related_name='tags')
+    tags = models.ManyToManyField("TagPost", blank=True, related_name="tags")
 
     objects = models.Manager()
     published = PublishedManager()
@@ -49,8 +49,11 @@ class Category(models.Model):
 
 
 class TagPost(models.Model):
-    tag = models.CharField(max_length=100,  db_index=True)
+    tag = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404, redirect, render
 
-
-from women.models import Category, Women
+from women.models import Category, TagPost, Women
 
 menu = [
     {"title": "О сайте", "url_name": "about"},
@@ -92,4 +91,18 @@ def show_category(request, cat_slug):
         "posts": posts,
         "cat_selected": category.pk,
     }
+    return render(request, "women/index.html", context=data)
+
+
+def show_tag_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+
+    data = {
+        "title": f"Тег {tag.tag}",
+        "menu": menu,
+        "posts": posts,
+        "cat_selected": None,
+    }
+
     return render(request, "women/index.html", context=data)
